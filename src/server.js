927167d45bf798ccdef17f98d7ce16df413b53da
muaz-kedir <kedirmuaz954@gmail.com>
2026-02-11 16:41:36 +0300

    Fix port binding for Render deployment

--- src/server.js
+++ src/server.js
@@ -9,6 +9,13 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
+// app.listen(PORT, () => {
+//   console.log(`✅ Server running on port ${PORT}`);
+// });
+
+
+// const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
